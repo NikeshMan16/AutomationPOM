24a770c74a6fea4fbad1adc18ef0cd47002ca9af
NikeshMan16 <nikeshmanshrestha@gmail.com>
2025-04-06 21:39:10 +0545

    variable files added

---
+++ variables_urls.py
@@ -0,0 +1,16 @@
+#TEST data variables
+
+valid_username = 'standard_user'
+valid_password = "secret_sauce"
+locked_out_user = 'locked_out_user'
+invalid_username = 'johndiss'
+invalid_password = 'ucantseeme'
+
+
+#URLS BEING DEFINED
+url_inventory_page ="https://www.saucedemo.com/inventory.html"
+url_cart_page = "https://www.saucedemo.com/cart.html"
+url_checkout_page_one = 'https://www.saucedemo.com/checkout-step-one.html'
+url_checkout_page_two = 'https://www.saucedemo.com/checkout-step-two.html'
+url_checkout_complete = 'https://www.saucedemo.com/checkout-complete.html'
+
